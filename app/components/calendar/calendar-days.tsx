1a2c366b8e00960118f6959fdab429751c668fd6
Romel Rey Silva <sromelrey@gmail.com>
2024-12-21 17:07:11 +0800

    feat(calendar): added a blur effect when 1 card is hovered

--- app/components/calendar/calendar-days.tsx
+++ app/components/calendar/calendar-days.tsx
@@ -1,20 +1,39 @@
 import "@/app/styles/calendar.scss";
+import { useState } from "react";
 
 export default function CalendarDays({ weeks }: { weeks: any[] }) {
+  const [hoveredIndex, setHoveredIndex] = useState<{
+    week: number;
+    day: number;
+  } | null>(null);
+
   return (
     <div className='calendar'>
       <table>
-        {weeks.map((week, weekIndex) => (
-          <tr key={weekIndex}>
-            {week.map((weekDay: any, dayIndex: any) => (
-              <td key={dayIndex}>
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  {weekDay.date.split("-")[2]}
-                </div>
-              </td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {weeks.map((week, weekIndex) => (
+            <tr key={weekIndex}>
+              {week.map((weekDay: any, dayIndex: any) => (
+                <td
+                  key={dayIndex}
+                  className={`wrapper ${
+                    hoveredIndex &&
+                    (hoveredIndex.week !== weekIndex ||
+                      hoveredIndex.day !== dayIndex)
+                      ? "blur"
+                      : ""
+                  }`}
+                  onMouseEnter={() =>
+                    setHoveredIndex({ week: weekIndex, day: dayIndex })
+                  }
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <div className='card'>{weekDay.date.split("-")[2]}</div>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
